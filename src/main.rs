bd7fe14012a8866e5e36623f2fec5f9f7e4919d5
Pirmin Kalberer <pka@sourcepole.ch>
2018-11-15 23:00:46 +0100

    Add Null Sink

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,21 @@ extern crate actix;
 extern crate futures;
 extern crate tokio;
 
+mod null;
+
 use actix::prelude::*;
 use futures::Future;
+use null::{NullSink, PutTile};
 
-/// Define `Ping` message
-struct Ping(usize);
-
-impl Message for Ping {
-    type Result = usize;
-}
-
-/// Actor
-struct MyActor {
-    count: usize,
-}
-
-/// Declare actor and its context
-impl Actor for MyActor {
-    type Context = Context<Self>;
-}
-
-/// Handler for `Ping` message
-impl Handler<Ping> for MyActor {
-    type Result = usize;
-
-    fn handle(&mut self, msg: Ping, _: &mut Context<Self>) -> Self::Result {
-        self.count += msg.0;
-        self.count
-    }
-}
 
 fn main() {
     // start system, this is required step
     System::run(|| {
         // start new actor
-        let addr = MyActor { count: 10 }.start();
+        let addr = NullSink { count: 10 }.start();
 
         // send message and get future for result
-        let res = addr.send(Ping(10));
+        let res = addr.send(PutTile{ x: 10, y: 0, z: 0, data: Vec::new() });
 
         // handle() returns tokio handle
         tokio::spawn(
---
+++ src/null.rs
@@ -0,0 +1,131 @@
+/// tilelive-null, A noop sink
+
+use actix::prelude::*;
+
+///  https://github.com/mapbox/tilelive/blob/master/API.md 
+///
+/// ```javascript
+/// function Tilesink() {
+///     // Constructor is identical to the the Tilesource constructor.
+///     // Tilesink and Tilesource should be one object.
+/// }
+/// ```
+///
+/// ```javascript
+/// Tilesink.prototype.startWriting = function(callback) {
+///     // Opens the Tilestore in write mode.
+///     // Call the callback function when done.
+///     function callback(err) {
+///         // err is null when write mode could be established successfully.
+///         // err is an Error object otherwise.
+///     }
+///
+///     // This function must be reentrant: Write mode may only be ended after the
+///     // same number of calls to .stopWriting(). Use a counter to keep track of
+///     // how often write mode was started.
+/// };
+/// ```
+///
+/// ```javascript
+/// Tilesink.prototype.stopWriting = function(callback) {
+///     // Ends the write mode.
+///     // Call the callback function when the request was successfully completed.
+///     // This doesn't mean that write mode has been ended (see below).
+///     function callback(err) {
+///         // err is null when write mode could be established successfully.
+///         // err is an Error object otherwise.
+///     }
+///
+///     // When caching and batch-writing tiles, they must be committed to the tile
+///     // store when this function is called, even when write mode is not ended.
+///     // This is true for grids as well.
+///
+///     // This function must be reentrant: Write mode may only be ended after the
+///     // same number of calls to .stopWriting(). Use a counter to keep track of
+///     // how often write mode was started.
+/// };
+/// ```
+///
+/// ```javascript
+/// Tilesink.prototype.putInfo = function(info, callback) {
+///     // Stores metadata into the tilesink. Info is a key-value hash with metadata.
+///     // Implementations may decide to reject invalid keys.
+///
+///     function callback(err) {
+///         // err is null when the metadata could be written successfully.
+///         // err is an Error object otherwise.
+///     }
+/// };
+/// ```
+///
+/// ```javascript
+/// Tilesink.prototype.putTile = function(z, x, y, tile, callback) {
+///     // Stores a tile into the data store. Parameters are in XYZ format.
+///     // `tile` must be a Buffer containing the compressed image.
+///
+///     function callback(err) {
+///         // err is null when the write request was received successfully.
+///         // This doesn't mean that the tile was already written to the data store.
+///         // err is an Error object otherwise.
+///     }
+///
+///     // Implementations may decide to cache multiple tile requests and only
+///     // commit them to the data store periodically to improve performance.
+///     // Therefore, users MUST NOT rely on this function to persist changes.
+///     // If you want to make sure that all changes are persisted, call
+///     // .stopWriting().
+/// };
+/// ```
+///
+/// ```javascript
+/// Tilesink.prototype.putGrid = function(z, x, y, grid, callback) {
+///     // Identical to .putTile(), but grid is a JS hash containing the grid.
+/// };
+
+
+pub struct PutTile {
+    pub z: usize,
+    pub x: usize,
+    pub y: usize,
+    pub data: Vec<u8>,
+}
+
+impl Message for PutTile {
+    type Result = usize;
+}
+
+/// Actor
+pub struct NullSink {
+    pub count: usize,
+}
+
+/// Declare actor and its context
+impl Actor for NullSink {
+    type Context = Context<Self>;
+}
+
+/// Handler for `PutTile` message
+impl Handler<PutTile> for NullSink {
+    type Result = usize;
+
+    fn handle(&mut self, msg: PutTile, _: &mut Context<Self>) -> Self::Result {
+        self.count += msg.x;
+        self.count
+    }
+}
+
+// var Null = function(uri, callback) {
+//   return setImmediate(callback, null, this);
+// };
+
+// Null.registerProtocols = function(tilelive) {
+//   tilelive.protocols["null:"] = Null;
+// };
+
+// Null.prototype.putTile = function(z, x, y, data, callback) {
+//   return setImmediate(callback);
+// };
+
+// Null.prototype.putInfo = function(obj, callback) {
+//   return setImmediate(callback);
+// };
