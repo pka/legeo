74196891e4146fc0c2b9250668e925f5bc386f11
Pirmin Kalberer <pka@sourcepole.ch>
2018-12-30 09:58:35 +0100

    Get source/dest URL's from command line

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,16 @@ pub mod tilesource;
 
 use crate::operation::{tile_copy, TileInput, TileOutput};
 use ::actix::prelude::*;
+use std::env;
 
+// Call example: legeo 'file:///tmp/legeo?filetype=pbf' 'file:///tmp/legeoout?filetype=pbf'
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let srcuri = args.get(1).expect("source-url missing").clone();
+    let dsturi = args.get(2).expect("dest-url missing").clone();
     System::run(|| {
-        let src = TileInput::from_uri("file:///tmp/legeo?filetype=txt".to_string());
-        let dst = TileOutput::from_uri("file:///tmp/legeoout?filetype=txt".to_string());
+        let src = TileInput::from_uri(srcuri);
+        let dst = TileOutput::from_uri(dsturi);
         tile_copy(src, dst);
 
         System::current().stop();
