8ed11bd1459b8f5629da66ac61303db9e734b8cc
Pirmin Kalberer <pka@sourcepole.ch>
2019-01-03 21:39:26 +0100

    Use StructOpt for argument parsing

--- legeo-cli/src/main.rs
+++ legeo-cli/src/main.rs
@@ -2,16 +2,20 @@ mod registry;
 
 use ::actix::prelude::*;
 use legeo::operation::tile_copy;
-use std::env;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    srcuri: String,
+    dsturi: String,
+}
 
 // Call example: legeo 'file:///tmp/legeo?filetype=pbf' 'file:///tmp/legeoout?filetype=pbf'
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let srcuri = args.get(1).expect("source-url missing").clone();
-    let dsturi = args.get(2).expect("dest-url missing").clone();
+    let args = Cli::from_args();
     System::run(|| {
-        let src = registry::TileInput::from_uri(srcuri);
-        let dst = registry::TileOutput::from_uri(dsturi);
+        let src = registry::TileInput::from_uri(args.srcuri);
+        let dst = registry::TileOutput::from_uri(args.dsturi);
         tile_copy(src, dst);
 
         System::current().stop();
